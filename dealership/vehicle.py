a275cf54d672e39e8a5a4348c39b515a1a64dfe4
nmoody9899 <63110489+nmoody9899@users.noreply.github.com>
2021-11-18 18:34:39 -0800

    Initialize vehicle class and properties

---
+++ dealership/vehicle.py
@@ -0,0 +1,31 @@
+class Vehicle:
+    def __init__(self, year: int, make: str, model: str, price: float, mileage: int):
+        self.__year = year
+        self.__make = make
+        self.__model = model
+        self.__price = price
+        self.__mileage = mileage
+
+    @property
+    def year(self):
+        return self.__year
+
+    @property
+    def make(self):
+        return self.__make
+
+    @property
+    def model(self):
+        return self.__model
+
+    @property
+    def price(self):
+        return self.__price
+
+    @property
+    def mileage(self):
+        return self.__mileage 
+
+    @property
+    def year_make_mode(self):
+        return f'{self.__year} {self.__make} {self.__model}'
